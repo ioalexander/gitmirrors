11033e80e031969e498c3b64976664c73f3e5e68
Alexander <io.alexander.io@pm.me>
2025-07-30 19:57:25 +0200

    Added CORS

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,6 +10,7 @@ mod models;
 mod schema;
 mod utils;
 use futures::FutureExt;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 use std::{panic::AssertUnwindSafe, time::Duration};
 
 #[macro_use]
@@ -23,6 +24,22 @@ fn health() -> &'static str {
 #[launch]
 async fn rocket() -> _ {
     dotenv().ok();
+    let cors_urls = dotenv::var("CORS_URL").unwrap_or_default();
+    let cors_origins: Vec<&str> = cors_urls
+        .split(',')
+        .map(str::trim)
+        .filter(|s| !s.is_empty())
+        .collect();
+
+    let cors_allowed_origins = AllowedOrigins::some_exact(&cors_origins);
+    let cors = CorsOptions {
+        allowed_origins: cors_allowed_origins,
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("Failed to create CORS fairing");
+
     let database_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = db::init_pool(&database_url);
 
@@ -57,6 +74,7 @@ async fn rocket() -> _ {
     });
 
     rocket::build()
+        .attach(cors)
         .manage(pool)
         .configure(
             rocket::Config::figment().merge((
